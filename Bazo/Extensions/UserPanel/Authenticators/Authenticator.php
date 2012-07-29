01d7321f92dcb739073add26a85347a2c6feef5f
bazo <martin@bazo.sk>
2012-07-29 20:55:53 +0200

    more refuctoring

--- Bazo/Extensions/UserPanel/Authenticators/Authenticator.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Bazo\Extensions\UserPanel\Authenticators;
-/**
- *
- * @author Martin
- */
-interface Authenticator extends Nette\Security\IAuthenticator
-{
-}
--- Bazo/Extensions/UserPanel/Providers/ArrayProvider.php
+++ Bazo/Extensions/UserPanel/Providers/ArrayProvider.php
@@ -5,7 +5,21 @@ namespace Bazo\Extensions\UserPanel\Providers;
  *
  * @author Martin
  */
-class ArrayProvider implements CredentialsProvider
+class ArrayProvider implements CredentialsProvider, \IteratorAggregate
 {
+	private
+		/** @var array */	
+		$array
+	;
 	
+	function __construct($array)
+	{
+		$this->array = $array;
+	}
+	
+	public function getIterator()
+	{
+		return new \ArrayIterator($this->array);
+	}
+
 }
